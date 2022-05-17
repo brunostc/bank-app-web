7f4a12fcc5fd6123bf4db6b4cb3706a799babcde
Bruno Stacheski <stacheskibruno@gmail.com>
2022-05-17 16:15:53 -0300

    Added metronic theme provider.

---
+++ src/_metronic/layout/MenuConfig.js
@@ -0,0 +1,42 @@
+export default {
+	header: {
+		self: {},
+		items: [
+			{
+			},
+		]
+	},
+	aside: {
+		self: {},
+		items: [
+			{
+				title: "Balance",
+				root: true,
+				icon: "fas fa-tachometer-alt",
+				page: "dashboard",
+				bullet: "dot"
+			},
+			{
+				title: "Checks",
+				root: true,
+				icon: "fas fa-money-check",
+				page: "deposit",
+				bullet: "dot"
+			},
+			{
+				title: "Purchases",
+				root: true,
+				icon: "fas fa-shopping-bag",
+				page: "purchase",
+				bullet: "dot"
+			},
+			{
+				title: "Transactions",
+				root: true,
+				icon: "fas fa-comments-dollar",
+				page: "transaction",
+				bullet: "dot"
+			},
+		]
+	}
+};
---
+++ src/_metronic/layout/MenuConfigAdmin.js
@@ -0,0 +1,28 @@
+export default {
+	header: {
+		self: {},
+		items: [
+			{
+			},
+		]
+	},
+	aside: {
+		self: {},
+		items: [
+			{
+				title: "Balance",
+				root: true,
+				icon: "fas fa-tachometer-alt",
+				page: "dashboard",
+				bullet: "dot"
+			},
+			{
+				title: "Checks",
+				root: true,
+				icon: "fas fa-money-check",
+				page: "deposit",
+				bullet: "dot"
+			},
+		]
+	}
+};
